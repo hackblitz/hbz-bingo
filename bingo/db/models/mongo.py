b23005bfe13f368fc3e4f8580a3f313db2601ab9
Satheesh Kumar <mail@satheesh.dev>
2022-06-05 20:14:49 +0530

    doc: add comments to mongo model

--- bingo/db/models/mongo.py
+++ bingo/db/models/mongo.py
@@ -1,10 +1,10 @@
 from datetime import datetime, timezone
 from typing import Optional
 
+from bingo.db.exceptions import IntegrityError
 from bingo.db.fields import Field
 from bingo.db.models import BaseConfig, BaseModel
 from bingo.types import Identifier
-from bson import ObjectId
 
 
 def __metaclass_resolver__(*classes):
@@ -18,6 +18,10 @@ def __metaclass_resolver__(*classes):
 
 
 class MongoModelManager:
+    """
+    Model manager class for mongodb. This will be available in the objects attribute in the MongoModel.
+    """
+
     def __init__(self, model_class):
         self.model_class = model_class
 
@@ -28,6 +32,10 @@ class MongoModelManager:
         return connection["questions"][self.model_class.get_collection_name()]
 
     async def get(self, **kwargs):
+        """
+        Find one document from the collection with the given kwargs
+        """
+
         collection = await self.get_collection()
         id = kwargs.get("id")
 
@@ -41,6 +49,10 @@ class MongoModelManager:
             return self.model_class(**obj)
 
     async def filter(self, **kwargs):
+        """
+        Find multiple documents from the collection with the given kwargs
+        """
+
         collection = await self.get_collection()
         objs = collection.find(kwargs)
 
@@ -48,6 +60,10 @@ class MongoModelManager:
             return [self.model_class(**obj) async for obj in objs]
 
     async def create(self, **kwargs):
+        """
+        Create document in the collection with given kwargs
+        """
+
         obj = self.model_class(**kwargs)
 
         if hasattr(self.model_class, "Meta"):
@@ -61,7 +77,9 @@ class MongoModelManager:
                 if len(duplicate_kwargs) == len(
                     self.model_class.Meta.unique_together
                 ) and await self.get(**duplicate_kwargs):
-                    raise Exception("Integrity error")
+                    raise IntegrityError(
+                        f"Duplicate element found with identifier {str(duplicate_kwargs)}"
+                    )
             elif hasattr(self.model_class.Meta, "unique"):
                 if await self.get(
                     **{
@@ -70,7 +88,9 @@ class MongoModelManager:
                         ]
                     }
                 ):
-                    raise Exception("Integrity error")
+                    raise IntegrityError(
+                        f"Duplicate element found with identifier {str({self.model_class.Meta.unique: kwargs[self.model_class.Meta.unique]})}"
+                    )
 
         collection = await self.get_collection()
         obj.last_updated = datetime.now()
@@ -84,6 +104,10 @@ class MongoModelManager:
             return str(new_obj.inserted_id)
 
     async def count(self, **kwargs):
+        """
+        Number of documents in the collection with the given kwargs
+        """
+
         collection = await self.get_collection()
         return await collection.count_documents(kwargs)
 
@@ -100,6 +124,9 @@ class MongoModelMeta(object, metaclass=MongoObjectMeta):
 
 
 class MongoModel(__metaclass_resolver__(BaseModel, MongoModelMeta)):
+    """
+    Base model class for mongodb
+    """
 
     id: Optional[Identifier] = Field(alias="_id")
     last_updated: datetime = Field(default=None, alias="_last_updated")
@@ -111,9 +138,12 @@ class MongoModel(__metaclass_resolver__(BaseModel, MongoModelMeta)):
             .isoformat()
             .replace("+00:00", "Z"),
             Identifier: str,
-            ObjectId: str,
         }
 
     @classmethod
     def get_collection_name(cls):
+        """
+        Collection on which the model with query upon
+        """
+
         return f"{cls.__name__.lower()}s"
